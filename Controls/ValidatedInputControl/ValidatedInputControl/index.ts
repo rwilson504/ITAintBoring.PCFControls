534a8a08ecf3b32ff2f96ff4f08959dcaaedf665
Rick Wilson <me@richardawilson.com>
2019-10-04 11:03:19 -0400

    Custom Error Message and Dispaly Option

--- Controls/ValidatedInputControl/ValidatedInputControl/index.ts
+++ Controls/ValidatedInputControl/ValidatedInputControl/index.ts
@@ -5,6 +5,10 @@ export class ValidatedInputControl implements ComponentFramework.StandardControl
   private _value: string;
   // RegEx to test against
   private _regEx: RegExp | null = null;
+  // Error message to dipslay if RegEx test fails
+  private _errorMessage: string;
+  // Show error if the value of the input field is emty
+  private _showErrorOnEmptyValue: boolean | null = false;
   // PCF framework delegate which will be assigned to this object which would be called whenever any update happens. 
   private _notifyOutputChanged: () => void;
   // label element created as part of this control
@@ -27,12 +31,12 @@ export class ValidatedInputControl implements ComponentFramework.StandardControl
  
   public setErrorState(value: string)
   {
-	if (this._regEx == null || this._regEx.test(value)) {
+	if ((!value && !this._showErrorOnEmptyValue) || this._regEx == null || this._regEx.test(value)) {
 		this.labelElement.innerHTML = "";
 		this._isError = false;
 	}
     else {
-		this.labelElement.innerHTML = "Incorrect format!";
+		this.labelElement.innerHTML = this._errorMessage;
 		this._isError = true;
 	}
 	return this._isError;
@@ -42,24 +46,29 @@ export class ValidatedInputControl implements ComponentFramework.StandardControl
 	  debugger;
     this._context = context;
     this._container = document.createElement("div");
-	this._container
+	  this._container
     this._notifyOutputChanged = notifyOutputChanged;
     this._blurHandler = this.blurHandler.bind(this);
-	this._inputHandler = this.inputHandler.bind(this);
+	  this._inputHandler = this.inputHandler.bind(this);
     // creating HTML elements for the input type range and binding it to the function which refreshes the control data
     this.inputElement = document.createElement("input");
     this.inputElement.setAttribute("type", "text");
     this.inputElement.addEventListener("blur", this._blurHandler);
-	this.inputElement.addEventListener("input", this._inputHandler);
+	  this.inputElement.addEventListener("input", this._inputHandler);
     this._value = context.parameters.value.raw;
 	
-	if(context.parameters.regEx != null)
+	if(context.parameters.regEx)
 	   this._regEx = new RegExp(context.parameters.regEx.raw);
    
-    var currentValue = context.parameters.value.formatted ? context.parameters.value.formatted : "0";
-    this.inputElement.setAttribute("value", currentValue);
-	this.labelElement = document.createElement("label");
+  if(context.parameters.errorMessage)
+    this._errorMessage = context.parameters.errorMessage.raw;
     
+  this._showErrorOnEmptyValue = (context.parameters.showErrorOnEmptyValue && context.parameters.showErrorOnEmptyValue.raw && context.parameters.showErrorOnEmptyValue.raw.toLowerCase() === 'false') ? false : true;
+
+  var currentValue = context.parameters.value.formatted ? context.parameters.value.formatted : "0";
+  this.inputElement.setAttribute("value", currentValue);
+	this.labelElement = document.createElement("label");
+  
 	this.setErrorState(currentValue);
     // appending the HTML elements to the control's HTML container element.
     this._container.appendChild(this.inputElement);
